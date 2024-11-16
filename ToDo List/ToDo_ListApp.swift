caa678b991aad5db1dc92ca332591cb699f9ffea
Evgeniy <borovoyevgen@gmail.com>
2024-11-16 23:28:55 +0400

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by Evgeniy Borovoy on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
